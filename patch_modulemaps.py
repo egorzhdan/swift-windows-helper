3eea661e319c8872b31ac7edc518bf5d0d99817d
Egor Zhdan <egor.zhdan@gmail.com>
2020-11-16 21:10:14 +0300

    Patch `visualc.modulemap` as well

--- patch_modulemaps.py
+++ patch_modulemaps.py
@@ -10,16 +10,21 @@ sdk_path = sdks_path + "\\" + max(os.listdir(sdks_path))
 print("Found Windows SDK at", sdk_path)
 
 symlink_path = toolchain_path + rel_path + "\\winsdk"
-print("Creating symlink at", symlink_path)
+vc_symlink_path = toolchain_path + rel_path + "\\vc"
+
+print("Creating symlinks at", symlink_path, vc_symlink_path)
 os.system("mklink /d \"" + symlink_path + "\" \"" + sdk_path + "\"")
+os.system("mklink /d \"" + vc_symlink_path + "\" \"%VCToolsInstallDir%/include\"")
 
 modulemap_path = toolchain_path + rel_path + "\\glibc.modulemap"
 print("Creating modulemap at", modulemap_path)
+visualc_modulemap = open(toolchain_path + rel_path + "\\visualc.modulemap", "r").read()
 winsdk_modulemap = open(toolchain_path + rel_path + "\\winsdk.modulemap", "r").read()
 ucrt_modulemap = open(toolchain_path + rel_path + "\\ucrt.modulemap", "r").read()
 
 f = open(modulemap_path, "w")
-f.write(winsdk_modulemap.replace("header \"", "header \"winsdk/um/") +
+f.write(visualc_modulemap.replace("header \"", "header \"vc/") +
+        winsdk_modulemap.replace("header \"", "header \"winsdk/um/") +
         ucrt_modulemap.replace("header \"", "header \"winsdk/ucrt/"))
 f.close()
 
