81f4ae99fd3017861ead79073b494fbf52a1b459
Egor Zhdan <egor.zhdan@gmail.com>
2020-11-30 23:16:55 +0300

    Avoid using `%VCToolsInstallDir%` macro
    it might not be configured in some environments

--- patch_modulemaps.py
+++ patch_modulemaps.py
@@ -9,12 +9,16 @@ sdks_path = "C:\\Program Files (x86)\\Windows Kits\\10\\Include"
 sdk_path = sdks_path + "\\" + max(os.listdir(sdks_path))
 print("Found Windows SDK at", sdk_path)
 
+vcs_path = "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community\\VC\\Tools\\MSVC"
+vc_path = vcs_path + "\\" + max(os.listdir(vcs_path)) + "\\include"
+print("Found VC at", vc_path)
+
 symlink_path = toolchain_path + rel_path + "\\winsdk"
 vc_symlink_path = toolchain_path + rel_path + "\\vc"
 
 print("Creating symlinks at", symlink_path, vc_symlink_path)
 os.system("mklink /d \"" + symlink_path + "\" \"" + sdk_path + "\"")
-os.system("mklink /d \"" + vc_symlink_path + "\" \"%VCToolsInstallDir%/include\"")
+os.system("mklink /d \"" + vc_symlink_path + "\" \"" + vc_path + "\"")
 
 modulemap_path = toolchain_path + rel_path + "\\glibc.modulemap"
 print("Creating modulemap at", modulemap_path)
