89b27146b0e32b3a43e241343197784a88b013b8
Egor Zhdan <egor.zhdan@gmail.com>
2020-12-01 21:22:26 +0300

    Patch SwiftPM extra compiler flags
    Add the `-I`/`-L` flags to workaround the compiler error:
    ```
    error: unable to load standard library for target 'x86_64-unknown-windows-msvc'
    ```

---
+++ patch_swiftpm_flags.py
@@ -0,0 +1,28 @@
+import sys
+
+platform_path = sys.argv[1]  # C:\Library\Developer\Platforms\Windows.platform
+print("Patching platform at", platform_path)
+
+info_plist_path = platform_path + "\\Info.plist"
+
+f = open(info_plist_path, "r")
+info_plist = f.read()
+f.close()
+
+flags = """
+<string>-I</string>
+<string>{platform}\\Developer\\SDKs\\Windows.sdk\\usr\\lib\\swift</string>
+
+<string>-I</string>
+<string>{platform}\\Developer\\SDKs\\Windows.sdk\\usr\\lib\\swift\\windows</string>
+
+<string>-L</string>
+<string>{platform}\\Developer\\SDKs\\Windows.sdk\\usr\\lib\\swift\\windows</string>
+""".format(platform=platform_path)
+
+f = open(info_plist_path, "w")
+f.write(info_plist.replace("<array>", "<array>" + flags))
+f.close()
+
+print()
+print("Done!")
