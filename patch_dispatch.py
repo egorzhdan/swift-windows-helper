39263205788a93826655c146cda16866ecbe9f54
Egor Zhdan <egor.zhdan@gmail.com>
2021-01-25 19:22:46 +0300

    Patch other include dirs as well

--- patch_dispatch.py
+++ patch_dispatch.py
@@ -2,6 +2,19 @@ import sys
 import shutil
 
 sdk_path = sys.argv[1]  # C:\Library\Developer\Platforms\Windows.platform\Developer\SDKs\Windows.sdk
-dispatch_dir = sdk_path + "\\usr\\lib\\swift\\dispatch"
+
 include_dir = sdk_path + "\\usr\\include"
-shutil.move(dispatch_dir, include_dir)
+
+dir_names = [
+    "Block",
+    "dispatch",
+    "os",
+]
+
+for dir_name in dir_names:
+    current_dir = sdk_path + "\\usr\\lib\\swift\\" + dir_name
+    print("Moving", current_dir, "to", include_dir)
+    shutil.move(current_dir, include_dir)
+
+print()
+print("Done!")
